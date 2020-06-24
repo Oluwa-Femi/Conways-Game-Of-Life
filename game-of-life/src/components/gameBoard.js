be6e4d35f09dda6197ec1ae6cb7af7fc31d2e584
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-06-24 16:31:16 +0100

    Button added to start or stop game simulation

--- game-of-life/src/components/gameBoard.js
+++ game-of-life/src/components/gameBoard.js
@@ -6,44 +6,54 @@ class Board extends Component {
   };
 
   gridState = () => {
-    for (let i=0; i<300; i+=20){
-        for (let j=0; j<300; j+=20){
-        this.setState({[`${i/20},${j/20}`]: "kpai"})
-        }
+    for (let i = 0; i < 300; i += 20) {
+      for (let j = 0; j < 300; j += 20) {
+        this.setState({ [`${i / 20},${j / 20}`]: "kpai" });
+      }
     }
-}
+  };
 
-handleClick = (e) => {
-  if (this.state.gameRun === false) {
-      const canvo = this.refs.canvas
+  handleClick = (e) => {
+    if (this.state.gameRun === false) {
+      const canvo = this.refs.canvas;
       const ctx = canvo.getContext("2d");
-      const pos = canvo.getBoundingClientRect()
-      const squareSize = 20
+      const pos = canvo.getBoundingClientRect();
+      const squareSize = 20;
       ctx.fillStyle = "red";
-      ctx.fillRect(e.clientX - pos.x - ((e.clientX - pos.x) % squareSize),
+      ctx.fillRect(
+        e.clientX - pos.x - ((e.clientX - pos.x) % squareSize),
         e.clientY - pos.y - ((e.clientY - pos.y) % squareSize),
         squareSize,
-        squareSize);
-        // console.log(`${(e.clientX - pos.x - (e.clientX - pos.x) % squareSize)/20},${ (e.clientY - pos.y - (e.clientY - pos.y) % squareSize)/20}`);
-        let tempCoord = `${(e.clientX - pos.x - (e.clientX - pos.x) % squareSize)/20},${ (e.clientY - pos.y - (e.clientY - pos.y) % squareSize)/20}`;
-        // console.log(this.state[`${tempCoord}`]);
-        this.setState({[`${tempCoord}`]: "living"});
-  }
-  else {console.log('Grid is not interactive while simulation is running');}
-}
+        squareSize
+      );
+      // console.log(`${(e.clientX - pos.x - (e.clientX - pos.x) % squareSize)/20},${ (e.clientY - pos.y - (e.clientY - pos.y) % squareSize)/20}`);
+      let tempCoord = `${
+        (e.clientX - pos.x - ((e.clientX - pos.x) % squareSize)) / 20
+      },${(e.clientY - pos.y - ((e.clientY - pos.y) % squareSize)) / 20}`;
+      // console.log(this.state[`${tempCoord}`]);
+      this.setState({ [`${tempCoord}`]: "living" });
+    } else {
+      console.log("Grid is not active while game is running");
+    }
+  };
 
-handleDoubleClick = (e) => {
-    const canvo = this.refs.canvas
+  handleDoubleClick = (e) => {
+    const canvo = this.refs.canvas;
     const ctx = canvo.getContext("2d");
-    const pos = canvo.getBoundingClientRect()
-    const squareSize = 20
+    const pos = canvo.getBoundingClientRect();
+    const squareSize = 20;
     ctx.fillStyle = "white";
-    ctx.fillRect(e.clientX - pos.x - ((e.clientX - pos.x) % squareSize),
+    ctx.fillRect(
+      e.clientX - pos.x - ((e.clientX - pos.x) % squareSize),
       e.clientY - pos.y - ((e.clientY - pos.y) % squareSize),
       squareSize,
-      squareSize);
-    }
+      squareSize
+    );
+  };
 
+  gameToggle = (e) => {
+    this.setState({ gameRun: !this.state.gameRun });
+  };
 
   componentDidMount() {
     this.gridState();
@@ -51,10 +61,9 @@ handleDoubleClick = (e) => {
     const ctx = canvas.getContext("2d");
     ctx.canvas.width = 500;
     ctx.canvas.height = 500;
-    ctx.strokeStyle = 'rgba(0,0,255)';
+    ctx.strokeStyle = "rgba(0,0,255)";
     for (let x = 0; x <= 500; x += 20) {
       for (let y = 0; y <= 500; y += 20) {
-
         ctx.moveTo(x, 0);
         ctx.lineTo(x, 500);
         ctx.stroke();
@@ -67,12 +76,17 @@ handleDoubleClick = (e) => {
 
   render() {
     return (
-      <div>
-        <canvas 
-        ref="canvas"
-        onClick={this.handleClick}
-        onDoubleClick={this.handleDoubleClick} 
+      <div className="cgof">
+        <canvas
+          ref="canvas"
+          onClick={this.handleClick}
+          onDoubleClick={this.handleDoubleClick}
         />
+        <div className="controls">
+          <button onClick={this.gameToggle}>
+            Start / Stop simulation
+          </button>
+        </div>
       </div>
     );
   }
