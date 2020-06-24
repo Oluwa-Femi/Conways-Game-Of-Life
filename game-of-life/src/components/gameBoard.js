af96b6479044badda6adfb913cd76c7c10e5488a
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-06-24 15:11:26 +0100

    Make each cell clickable

--- game-of-life/src/components/gameBoard.js
+++ game-of-life/src/components/gameBoard.js
@@ -3,13 +3,49 @@ import React, { Component } from "react";
 class Board extends Component {
   state = {};
 
+  gridState = () => {
+    for (let i=0; i<300; i+=20){
+        for (let j=0; j<300; j+=20){
+        this.setState({[`${i/20},${j/20}`]: "vollaa"})
+        }
+    }
+}
+
+handleClick = (e) => {
+    const canvo = this.refs.canvas
+    const ctx = canvo.getContext("2d");
+    const pos = canvo.getBoundingClientRect()
+    const squareSize = 20
+    ctx.fillStyle = "red";
+    ctx.fillRect(e.clientX - pos.x - ((e.clientX - pos.x) % squareSize),
+      e.clientY - pos.y - ((e.clientY - pos.y) % squareSize),
+      squareSize,
+      squareSize);
+}
+
+handleDoubleClick = (e) => {
+    const canvo = this.refs.canvas
+    const ctx = canvo.getContext("2d");
+    const pos = canvo.getBoundingClientRect()
+    const squareSize = 20
+    ctx.fillStyle = "white";
+    ctx.fillRect(e.clientX - pos.x - ((e.clientX - pos.x) % squareSize),
+      e.clientY - pos.y - ((e.clientY - pos.y) % squareSize),
+      squareSize,
+      squareSize);
+}
+
+
   componentDidMount() {
+    this.gridState();
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext("2d");
     ctx.canvas.width = 500;
     ctx.canvas.height = 500;
+    ctx.strokeStyle = 'rgba(0,0,255)';
     for (let x = 0; x <= 500; x += 20) {
       for (let y = 0; y <= 500; y += 20) {
+
         ctx.moveTo(x, 0);
         ctx.lineTo(x, 500);
         ctx.stroke();
@@ -23,7 +59,11 @@ class Board extends Component {
   render() {
     return (
       <div>
-        <canvas ref="canvas" />
+        <canvas 
+        ref="canvas"
+        onClick={this.handleClick}
+        onDoubleClick={this.handleDoubleClick} 
+        />
       </div>
     );
   }
