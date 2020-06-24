5d713ef68e79ea16351da29727e62afd79196a61
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-06-24 16:18:08 +0100

    Grid is not active while game is running

--- game-of-life/src/components/gameBoard.js
+++ game-of-life/src/components/gameBoard.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Board extends Component {
   state = {
-    // gameRun: false,
+    gameRun: false,
   };
 
   gridState = () => {
@@ -14,19 +14,22 @@ class Board extends Component {
 }
 
 handleClick = (e) => {
-    const canvo = this.refs.canvas
-    const ctx = canvo.getContext("2d");
-    const pos = canvo.getBoundingClientRect()
-    const squareSize = 20
-    ctx.fillStyle = "red";
-    ctx.fillRect(e.clientX - pos.x - ((e.clientX - pos.x) % squareSize),
-      e.clientY - pos.y - ((e.clientY - pos.y) % squareSize),
-      squareSize,
-      squareSize);
-      // console.log(`${(e.clientX - pos.x - (e.clientX - pos.x) % squareSize)/20},${ (e.clientY - pos.y - (e.clientY - pos.y) % squareSize)/20}`);
-      let tempCoord = `${(e.clientX - pos.x - (e.clientX - pos.x) % squareSize)/20},${ (e.clientY - pos.y - (e.clientY - pos.y) % squareSize)/20}`;
-      // console.log(this.state[`${tempCoord}`]);
-      this.setState({[`${tempCoord}`]: "living"});
+  if (this.state.gameRun === false) {
+      const canvo = this.refs.canvas
+      const ctx = canvo.getContext("2d");
+      const pos = canvo.getBoundingClientRect()
+      const squareSize = 20
+      ctx.fillStyle = "red";
+      ctx.fillRect(e.clientX - pos.x - ((e.clientX - pos.x) % squareSize),
+        e.clientY - pos.y - ((e.clientY - pos.y) % squareSize),
+        squareSize,
+        squareSize);
+        // console.log(`${(e.clientX - pos.x - (e.clientX - pos.x) % squareSize)/20},${ (e.clientY - pos.y - (e.clientY - pos.y) % squareSize)/20}`);
+        let tempCoord = `${(e.clientX - pos.x - (e.clientX - pos.x) % squareSize)/20},${ (e.clientY - pos.y - (e.clientY - pos.y) % squareSize)/20}`;
+        // console.log(this.state[`${tempCoord}`]);
+        this.setState({[`${tempCoord}`]: "living"});
+  }
+  else {console.log('Grid is not interactive while simulation is running');}
 }
 
 handleDoubleClick = (e) => {
